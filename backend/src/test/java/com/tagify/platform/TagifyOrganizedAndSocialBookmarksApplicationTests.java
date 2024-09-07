94b2d3b783306aff0b6461555e0500a98d8b3024
Kamil Woskowiak <kamilwoskowiak1@gmail.com>
2024-09-07 02:40:36 -0400

    first commit

---
+++ backend/src/test/java/com/tagify/platform/TagifyOrganizedAndSocialBookmarksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagify.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagifyOrganizedAndSocialBookmarksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
