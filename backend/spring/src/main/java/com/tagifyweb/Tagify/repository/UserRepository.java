060bba7cd5a296865a4d282ccdd56f8912b56a32
Kamil Woskowiak <kamilwoskowiak1@gmail.com>
2024-09-08 02:50:50 -0400

    user class complete, added user interface

---
+++ backend/spring/src/main/java/com/tagifyweb/Tagify/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tagifyweb.Tagify.repository;
+
+import com.tagifyweb.Tagify.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
