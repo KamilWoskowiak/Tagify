a0d139be4522fcf4dec16c5e61a8865c568a5d53
Kamil <76923760+kamilwoskowiak@users.noreply.github.com>
2024-09-07 21:59:22 -0400

    Springboot upload

---
+++ backend/spring/src/test/java/com/tagifyweb/Tagify/TagifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagifyweb.Tagify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
