8a381afc1fc01d8654497a6b02ff097c21529847
Kamil <76923760+KamilWoskowiak@users.noreply.github.com>
2024-09-07 21:59:22 -0400

    Springboot upload

---
+++ backend/spring/src/main/java/com/tagifyweb/Tagify/TagifyApplication.java
@@ -0,0 +1,13 @@
+package com.tagifyweb.Tagify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagifyApplication.class, args);
+	}
+
+}
