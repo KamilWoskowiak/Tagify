2c08036347565104185017d516574233520ec8db
Kamil Woskowiak <kamilwoskowiak1@gmail.com>
2024-09-09 02:06:14 -0400

    dto

---
+++ backend/spring/src/main/java/com/tagifyweb/Tagify/dto/LoginUserDto.java
@@ -0,0 +1,11 @@
+package com.tagifyweb.Tagify.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
---
+++ backend/spring/src/main/java/com/tagifyweb/Tagify/dto/RegisterUserDto.java
@@ -0,0 +1,12 @@
+package com.tagifyweb.Tagify.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
