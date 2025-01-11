1f997612149fca64be6e731a9939961ff3705d3e
KamilWoskowiak <kamilwoskowiak1@gmail.com>
2025-01-11 00:45:21 -0500

    start

---
+++ backend/TagifySystem/src/main/java/com/tagify/TagifySystem/TagifySystemApplication.java
@@ -0,0 +1,13 @@
+package com.tagify.TagifySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagifySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagifySystemApplication.class, args);
+	}
+
+}
---
+++ backend/TagifySystem/src/test/java/com/tagify/TagifySystem/TagifySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagify.TagifySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagifySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
